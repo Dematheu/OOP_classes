40aefd27ba3376ed0311df2731984d23a7f5acfe
Dematheu <arq.dematheu@gmail.com>
2023-06-05 17:30:44 -0300

    Day 3

---
+++ creational/singleton/monostate_2.py
@@ -0,0 +1,52 @@
+"""
+Monostate (ou Borg) - É uma variação do Singleton proposto
+por Alex Martelli que tem a intenção de garantir que o
+estado do objeto seja igual para todas as instâncias.
+"""
+from __future__ import annotations
+
+from typing import Dict
+
+
+class StringReprMixin:
+    def __str__(self) -> str:
+        params = ', '.join(
+            [f'{k}={v}' for k, v in self.__dict__.items()]
+        )
+        return f'{self.__class__.__name__}({params})'
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class MonoState(StringReprMixin):
+    _state: Dict = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        obj.__dict__ = cls._state
+        return obj
+
+    def __init__(self, nome=None, sobrenome=None) -> None:
+        if nome is not None:
+            self.nome = nome
+
+        if sobrenome is not None:
+            self.sobrenome = sobrenome
+
+
+class A(MonoState):
+    pass
+
+
+class B(A):
+    pass
+
+
+if __name__ == "__main__":
+    m1 = MonoState(nome='Lucas')
+    m2 = A(sobrenome='de Matheu')
+    m3 = B(sobrenome='de Matheu')
+    print(m1)
+    print(m2)
+    print(m3)
